bd1e77fe87499ba4cd5f6dd7ffcb3baff9d8534a
eroliver <eaoliver@mix.wvu.edu>
2020-06-18 15:58:11 -0400

    automation tests commit - deleting downloads

--- app.py
+++ app.py
@@ -11,8 +11,17 @@ import send2trash as send2trash
 
 # files = ['C:\Users\eoliver\Downloads']
 # os.system('"runas /user:USERNAME "C:/Python27/python.exe shell.py""')
-downloads = Path('C:/Users/eoliver/Downloads/BackupJobSummaryReport_2020-06-17-08-00-08.pdf')
+# downloads = Path('C:/Users/eoliver/Downloads/BackupJobSummaryReport_2020-06-17-08-00-08.pdf')
 # os.remove(downloads)
 #os.remove(downloads)
-downloads_modified = os.path.getmtime(downloads)
-print(downloads_modified)
+# downloads_modified = os.path.getmtime(downloads)
+# print(downloads_modified)
+
+basepath = 'C:/Users/eoliver/Downloads/'
+with os.scandir(basepath) as entries:
+    for entry in entries:
+        if entry.is_file():
+            print(entry.name)
+            os.remove(basepath+entry.name)
+
+
