4d11f1a69656e40e53e780c627dafb7d8c4ea237
eroliver <eaoliver@mix.wvu.edu>
2020-06-18 11:13:55 -0400

    automation tests commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import os
-import sys
-import win64com.shell.shell as shell
+import tkinter as tk
+import send2trash as send2trash
+#import win64com.shell.shell as shell
 
 # C:\Users\eoliver\Downloads
 #users = []
@@ -9,6 +10,9 @@ import win64com.shell.shell as shell
  #   append()
 
 # files = ['C:\Users\eoliver\Downloads']
-
-downloads = Path('C:/Users/eoliver/Downloads')
-os.remove(downloads)
+# os.system('"runas /user:USERNAME "C:/Python27/python.exe shell.py""')
+downloads = Path('C:/Users/eoliver/Downloads/BackupJobSummaryReport_2020-06-17-08-00-08.pdf')
+# os.remove(downloads)
+#os.remove(downloads)
+downloads_modified = os.path.getmtime(downloads)
+print(downloads_modified)
