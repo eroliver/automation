6b5cd6d1d3633d70fe1cd3cc258232790b603f69
eroliver <eaoliver@mix.wvu.edu>
2020-06-19 12:54:52 -0400

    automation tests commit - deleting downloads

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import time
 import tkinter as tk
 import send2trash as send2trash
 #import win64com.shell.shell as shell
@@ -17,11 +18,33 @@ import send2trash as send2trash
 # downloads_modified = os.path.getmtime(downloads)
 # print(downloads_modified)
 
-basepath = 'C:/Users/eoliver/Downloads/'
-with os.scandir(basepath) as entries:
-    for entry in entries:
-        if entry.is_file():
-            print(entry.name)
-            os.remove(basepath+entry.name)
+
+def clear_old_downloads():
+    base_path = 'C:/Users/eoliver/Downloads/'
+    with os.scandir(base_path) as entries:
+        for entry in entries:
+            if entry.is_file() and os.path.getmtime(entry) < time.time() - 31536000:
+                print(entry.name)
+                os.remove(base_path+entry.name)
 
 
+# add check for multiple drives and users
+def clear_old_recycle_bin():
+    base_path = 'C:/$Recycle.Bin/'
+    with os.scandir(base_path) as entries:
+        for entry in entries:
+            if entry.is_file() and os.path.getmtime(entry) < time.time() - 15768000:
+                print(entry.name)
+                os.remove(base_path + entry.name)
+
+
+def clear_old_recycle_bin_desktop():
+    base_path = 'C:/Users/eoliver/Desktop/'
+    with os.scandir(base_path) as entries:
+        for entry in entries:
+            if entry.is_file() and os.path.getmtime(entry) < time.time() - 100:
+                print(entry.name)
+                # os.remove(base_path + entry.name)
+
+
+clear_old_recycle_bin_desktop()
